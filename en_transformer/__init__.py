d7530e381fad9006106d4f439aecd18337b02b2e
Phil Wang <lucidrains@gmail.com>
2021-02-27 07:32:17 -0800

    add base attention module

---
+++ en_transformer/__init__.py
@@ -0,0 +1,1 @@
+from en_transformer.en_transformer import EGAT
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'En-transformer',
+  packages = find_packages(),
+  version = '0.0.1',
+  license='MIT',
+  description = 'E(n)-Equivariant Transformer',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/En-transformer',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'equivariance',
+    'transformer'
+  ],
+  install_requires=[
+    'einops>=0.3',
+    'torch>=1.6'
+  ],
+  setup_requires=[
+    'pytest-runner',
+  ],
+  tests_require=[
+    'pytest'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)

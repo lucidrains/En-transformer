ef55d97adb9cf9964abc98c001263a330522d280
Phil Wang <lucidrains@gmail.com>
2021-02-27 18:06:58 -0800

    first commit

--- tests/test_equivariance.py
+++ tests/test_equivariance.py
@@ -1,9 +1,14 @@
 import torch
 from en_transformer.utils import rot
-from en_transformer import EGAT
+from en_transformer import EnTransformer
 
-def test_egat_equivariance():
-    layer = EGAT(dim = 512, edge_dim = 4)
+def test_equivariance():
+    model = EnTransformer(
+        dim = 512,
+        depth = 4,
+        edge_dim = 4,
+        fourier_features = 2
+    )
 
     R = rot(*torch.rand(3))
     T = torch.randn(1, 1, 3)
@@ -12,8 +17,8 @@ def test_egat_equivariance():
     coors = torch.randn(1, 16, 3)
     edges = torch.randn(1, 16, 16, 4)
 
-    feats1, coors1 = layer(feats, coors @ R + T, edges)
-    feats2, coors2 = layer(feats, coors, edges)
+    feats1, coors1 = model(feats, coors @ R + T, edges)
+    feats2, coors2 = model(feats, coors, edges)
 
-    assert torch.allclose(feats1, feats2, atol = 1e-6), 'type 0 features are invariant'
-    assert torch.allclose(coors1, (coors2 @ R + T), atol = 1e-6), 'type 1 features are equivariant'
+    assert torch.allclose(feats1, feats2, atol = 1e-4), 'type 0 features are invariant'
+    assert torch.allclose(coors1, (coors2 @ R + T), atol = 1e-4), 'type 1 features are equivariant'
